665740fb43294dbb15410d2255915aabb6d7e139
andrej <andrejskoda@gmail.com>
2015-12-11 01:53:02 +0100

    removed unused classes

--- src/main/java/sk/assignement/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package sk.assignement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sk.assignement.model.Item;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
